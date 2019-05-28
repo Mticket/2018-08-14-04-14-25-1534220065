ee28058ac22ca8a0d36b8a94d2f57dba83448c6a
isaiah <mticket@hotmail.com>
2019-05-28 20:37:31 +0800

    update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,24 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(x) {
+	
+	var lcd_number = '._....._.._....._.._.._.._.._.|.|..|._|._||_||_.|_...||_||_||_|..||_.._|..|._||_|..||_|..|';
+	var lcd = '';
+	for(var i = 0; i < 3; i++)
+	{
+		
+		for(var j = 0; j < x.length; j++)
+		{
+			var number = Number(x[j]);
+			lcd = lcd + lcd_number.substr(30 * i + 3 * number , 3);
+			if( j != (x.length - 1))
+			{
+				lcd = lcd + ' ';
+			}
+			else
+			{
+				lcd = lcd + '\n';
+			}
+		}
+	}
+	return lcd;
+	
 };
